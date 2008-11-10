86c571840b2455b723cb364ea80d07e43be4b9f7
gdromard <gdromard@43615dec-af2d-11dd-a200-c3c65cc52ccb>
2008-11-10 13:52:31 +0000

    Share project 'picasaweb.album.downloader' into 'https://picasawebalbumdownloader.googlecode.com/svn'

---
+++ src/main/java/dromard/picasaweb/album/downloader/App.java
@@ -0,0 +1,13 @@
+package dromard.picasaweb.album.downloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
